84f42292dd3ceb4b2dcf1e978f15cc8555ddfc81
Sergey Bondarenko <sergey.bondarenko457@gmail.com>
2026-05-15 16:20:46 +0700

    add button style

--- wp-content/themes/shop-co/front-page.php
+++ wp-content/themes/shop-co/front-page.php
@@ -10,6 +10,32 @@ get_header();
 
 <main id="primary" class="site-main">
 	<div class="container">
+
+		<div class="" style="margin-block: 2rem;">
+			<button class="site-button">Button</button>
+			<a href="#" class="site-button">Link Button</a>
+
+			<button class="site-button site-button--white site-button--border">Button</button>
+			<a href="#" class="site-button site-button--white site-button--border">Link Button</a>
+
+			<button class="site-button site-button--white">Button</button>
+			<a href="#" class="site-button site-button--white">Link Button</a>
+
+			<button class="site-button site-button--small">Button</button>
+			<a href="#" class="site-button site-button--small">Link Button</a>
+
+			<button class="site-button site-button--square">
+				<?php echo ShopCo_Icons::settings(); ?>
+			</button>
+			<a href="#" class="site-button site-button--square">
+				<?php echo ShopCo_Icons::settings(); ?>
+			</a>
+			
+			<button class="site-button site-button--square" disabled>
+				<?php echo ShopCo_Icons::settings(); ?>
+			</button>
+		</div>
+
 		<?php
 		while ( have_posts() ) :
 			the_post();
--- wp-content/themes/shop-co/functions.php
+++ wp-content/themes/shop-co/functions.php
@@ -13,3 +13,4 @@ require get_template_directory() . '/inc/setup.php';
 require get_template_directory() . '/inc/enqueue.php';
 require get_template_directory() . '/inc/template-tags.php';
 require get_template_directory() . '/inc/woocommerce.php';
+require get_template_directory() . '/inc/icons.php';
